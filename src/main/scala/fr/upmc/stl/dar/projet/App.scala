859e7d68f7daf7c16a3e0358211da67a6e7a1c60
ThibCharpy <thibault.charpignon@wanadoo.fr>
2018-04-15 21:13:10 +0200

    finished ! :bee:

--- src/main/scala/fr/upmc/stl/dar/projet/App.scala
+++ src/main/scala/fr/upmc/stl/dar/projet/App.scala
@@ -1,9 +1,11 @@
 package fr.upmc.stl.dar.projet
 
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.ml.feature.{HashingTF, IDF, LabeledPoint}
-import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.sql._
+import org.apache.spark.ml.feature._
 
 object App {
 
@@ -25,32 +27,98 @@ object App {
     val dataFrame = sqlContext.read.option("multiline", "true").json("test.json").toDF()
     dataFrame.show()
 
-    val newTextCol = dataFrame.select("text").map(row => cleanStrings(row.getString(0)))
-    newTextCol.show()
+    val columns: Array[String] = dataFrame.columns
+    val reorderedColumnNames: Array[String] = Array("id","title","text","code","tags") // do the reordering you want
+    val orderedDF: DataFrame = dataFrame.select(reorderedColumnNames.head, reorderedColumnNames.tail: _*)
+
+    val wordsListClean = orderedDF.select("text")
+      .map(row => cleanWords(row.getString(0).split(" ")))
+    wordsListClean.show()
+
+    val tokenizerText = new Tokenizer().setInputCol("value").setOutputCol("words")
+    val wordsData = tokenizerText.transform(wordsListClean)
+    wordsData.show()
+    wordsData.printSchema()
+
 
     /*TF IDF over cleaned datas*/
-    val hashingTF = new HashingTF()
-    hashingTF.setNumFeatures(10000)
-    hashingTF.setInputCol("value")
-    hashingTF.setOutputCol("TF")
-    val tf: DataFrame = hashingTF.transform(newTextCol.na.drop(Array("value")))
-    tf.show()
-    tf.cache()
-    val idf = new IDF()
-    idf.setInputCol("TF")
-    idf.setOutputCol("TF-IDF")
-    val idf_temp = idf.fit(tf.na.drop(Array("TF")))
-    val tfidf: DataFrame = idf_temp.transform(tf)
+    val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(50000)
+    val featurizedWords = hashingTF.transform(wordsData)
+    featurizedWords.show()
 
-    tfidf.show()
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel = idf.fit(featurizedWords)
 
-    /* Préparation des données */
+    val rescaledWords = idfModel.transform(featurizedWords)
+    val datas = rescaledWords.select("words","features")
+    datas.show()
 
-    /* Classfication bayesienne */
+    val wordsIndexer = new StringIndexer().setInputCol("value").setOutputCol("indexedWords")
+      .fit(featurizedWords)
+    val res = wordsIndexer.transform(rescaledWords).toDF("label","words","rawFeatures","features","indexedWords")
+    res.show()
+    res.printSchema()
+
+    /*val labeledPoints = res.map(
+      row => {
+        val vector = row.getAs[Vector](3)
+        val id = row.getAs[Double](4)
+        LabeledPoint(id,vector)
+      }
+    )*/
+
+    val wordsIndexer2 = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+      .fit(res)
+
+    val featureIndexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("indexedFeatures")
+      .setMaxCategories(4)
+      .fit(res)
+
+    val Array(trainingData,testData) = res.randomSplit(Array(0.7,0.3), seed = 1234L)
+
+    // Train a RandomForest model.
+    val rf = new RandomForestClassifier()
+      .setLabelCol("indexedLabel")
+      .setFeaturesCol("indexedFeatures")
+      .setNumTrees(10)
+
+    // Convert indexed labels back to original labels.
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("predictedLabel")
+      .setLabels(wordsIndexer.labels)
+
+    // Chain indexers and forest in a Pipeline.
+    val pipeline = new Pipeline()
+      .setStages(Array(wordsIndexer2, featureIndexer, rf, labelConverter))
+
+    // Train model. This also runs the indexers.
+    val model = pipeline.fit(trainingData)
+
+    // Make predictions.
+    val predictions = model.transform(testData)
+
+    // Select example rows to display.
+    predictions.select("predictedLabel", "label", "features").show(5)
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("indexedLabel")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println("Test Error = " + (1.0 - accuracy))
+
+    val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+    println("Learned classification forest model:\n" + rfModel.toDebugString)
 
   }
 
-  def cleanStrings (string: String): Array[String] = {
+  def cleanWords (words: Array[String]): String = {
     val stopwords_list = Array(
       "a",  "about",  "above",  "after",  "again",  "against",  "all",  "am",  "an",  "and",  "any",  "are",  "arent",  "as",  "at",
      "be",  "because",  "been",  "before",  "being",  "below",  "between",  "both",  "but",  "by",
@@ -66,19 +134,19 @@ object App {
      "no",  "nor",  "not",  "now",
      "o",  "of",  "off",  "on",  "once",  "only",  "or",  "other",  "our",  "ours",  "ourselves",  "out",  "over",  "own",
      "r",  "re",
-     "s", "said",  "same",  "she",  "should",  "shouldnt",  "so",  "some",  "such",
+     "s", "said",  "same",  "she",  "should",  "shouldnt",  "so",  "some",  "such", "still",
      "t",  "than",  "that", "thats",  "the",  "their",  "theirs",  "them",  "themselves",  "then",  "there",  "these",  "they",  "this",  "those",  "through",  "to",  "too",
      "under",  "until",  "up",
      "very",
      "was",  "wasnt",  "we",  "were",  "werent",  "what",  "when",  "where",  "which",  "while",  "who",  "whom",  "why",  "will",  "with",  "wont",  "would",
      "y",  "yo ",  "your",  "yours",  "yourself",  "yourselves")
-    val stopwords_list_custom = Array("")
+    val stopwords_list_custom = Array("try","trying","need","know", "following")
     val stopwords = stopwords_list ++ stopwords_list_custom
 
-    val words = string.split("[ ]+?")
-    var res = words.filter( word => word.matches("[A-Za-z]+"))
-    res = res.filter( word => word.toLowerCase.length > 2 && !stopwords.contains(word.toLowerCase()))
-    res
+
+    val res = words.filterNot(_.isEmpty).filter(word =>
+      word.toLowerCase.length>2 && !stopwords.contains(word.toLowerCase) && word.matches("[A-Za-z]+"))
+    res.mkString(" ")
   }
 
 }
